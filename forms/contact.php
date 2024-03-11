9e53642ab71b039af9252031f586272cde9c4f75
ritatsousa <53748834+ritatsousa@users.noreply.github.com>
2024-03-11 14:35:58 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,18 +1,24 @@
 <?php
 $to = 'challengnegknow@gmail.com';
-$name = $_POST['name'];
-$from = $_POST['email'];
-$subject = $_POST['subject'];
-$message =  $_POST['message'];
+$name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
+$from = isset($_POST['email']) ? filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) : false;
+$subject = isset($_POST['subject']) ? htmlspecialchars($_POST['subject']) : '';
+$message = isset($_POST['message']) ? htmlspecialchars($_POST['message']) : '';
 
-if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
-    $headers = ['From' => ($name?"<$name> ":'').$from,
-            'X-Mailer' => 'PHP/' . phpversion()
-           ];
+if ($from) {
+    $headers = [
+        'From' => ($name ? "<$name> " : '') . $from,
+        'X-Mailer' => 'PHP/' . phpversion(),
+        'Content-Type' => 'text/plain; charset=utf-8', // Adjust content type as needed
+    ];
 
-    mail($to, $subject, $message."\r\n\r\nfrom: ".$_SERVER['REMOTE_ADDR'], $headers);
-    die('OK');
-    
+    $fullMessage = $message . "\r\n\r\nfrom: " . $_SERVER['REMOTE_ADDR'];
+
+    if (mail($to, $subject, $fullMessage, $headers)) {
+        die('OK');
+    } else {
+        die('Error sending email');
+    }
 } else {
     die('Invalid address');
 }
